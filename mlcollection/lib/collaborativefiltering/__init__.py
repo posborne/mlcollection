2d4bb2e7dd65984c5f0bf266e2025584f6443361
Paul Osborne <paul-osborne@bethel.edu>
2010-03-24 01:51:48 -0500

    Beginnings of collaborative filtering work (topic branch stub)

---
+++ mlcollection/lib/collaborativefiltering/__init__.py
@@ -0,0 +1,3 @@
+# TODO: implement a few different collaborative filtering algorithms and
+# then find some way of synthesizing the operations that are performed with
+# these algorithms into some easy-to-understand and use framework
